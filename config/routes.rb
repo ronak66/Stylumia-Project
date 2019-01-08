e4c53df19a13ec82f396a004f7352ca88dafcc01
Ronak Doshi <ronakvipul.doshi@iiitb.org>
2019-01-08 18:32:43 +0530

    Added Post and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :posts
+  # resources :comments
+  resources :posts do
+    resources :comments
+  end
   root 'static_pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/user/posts', to: 'static_pages#list', as: :post_list
+  
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_07_174541) do
+ActiveRecord::Schema.define(version: 2019_01_08_093406) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
